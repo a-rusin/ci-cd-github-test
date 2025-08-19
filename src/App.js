787005e2cd4c1d5416f1b5463cb56678b98ee635
Alexandr Rusin <i@arusin.ru>
2025-08-19 23:47:34 +0700

    sone fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello again again!</p>
+        <p>Hello again again and again!</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React!
         </a>
